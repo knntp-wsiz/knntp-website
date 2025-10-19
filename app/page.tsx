a543e95d519778fe554b24eee0cd5678412b8b1b
Eryk Kunasz <eryk.kunasz@gmail.com>
2025-10-19 15:58:24 +0200

    feat: add WSIiZ logos and update homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,40 @@ import { FaDiscord, FaFacebook, FaGithub } from "react-icons/fa";
 export default function Home() {
 	return (
 		<div className="flex flex-1 flex-col items-center justify-center">
-			<Image
-				src="/logo_white.png"
-				alt="KNNTP Logo"
-				width={500}
-				height={253.906}
-				className="hidden dark:block"
-			/>
-			<Image
-				src="/logo_black.png"
-				alt="KNNTP Logo"
-				width={500}
-				height={253.906}
-				className="dark:hidden"
-			/>
-			<p className="text-xl text-center">
-				Oficjalna strona koła naukowego nowych technologii programistycznych
-			</p>
+			<div className="flex justify-center items-center flex-col lg:flex-row">
+				<Image
+					src="/wsiz_logo_white.png"
+					alt="WSIiZ Logo"
+					width={491}
+					height={100}
+					className="hidden dark:block"
+				/>
+				<Image
+					src="/wsiz_logo_black.png"
+					alt="WSIiZ Logo"
+					width={491}
+					height={100}
+					className="dark:hidden"
+				/>
+
+				<Image
+					src="/logo_white.png"
+					alt="KNNTP Logo"
+					width={500}
+					height={253.906}
+					className="hidden dark:block"
+				/>
+				<Image
+					src="/logo_black.png"
+					alt="KNNTP Logo"
+					width={500}
+					height={253.906}
+					className="dark:hidden"
+				/>
+			</div>
+
+			<h1 className="text-4xl font-bold text-center mt-8">Linki:</h1>
+
 			<div className="flex justify-center space-x-4 mt-4">
 				<Link
 					href="https://www.facebook.com/netRzeszow/"
